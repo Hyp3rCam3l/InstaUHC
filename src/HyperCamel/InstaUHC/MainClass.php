8dac70a2031a981d606743afcf41e29511416ca6
Hyp3rCam3l <ajb2023@gmail.com>
2015-03-07 14:25:58 -0500

    Update MainClass.php
    Added public function ___

--- src/HyperCamel/InstaUHC/MainClass.php
+++ src/HyperCamel/InstaUHC/MainClass.php
@@ -5,5 +5,11 @@ namespace HyperCamel\InstaUHC
 use pocketmine\plugin\PluginBase
 
 class MainClass extends PluginBase{
-  
+  public function onEnable () {
+  }
+  public function  onLoad (){
+  }
+  public function onDisable () {
+  }
 }
+
